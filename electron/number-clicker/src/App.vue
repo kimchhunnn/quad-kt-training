e7566a620b07c5413ca574dc4bc5c56063fb10f5
kimchhunnn <76899805+kimchhunnn@users.noreply.github.com>
2021-01-04 14:01:52 +0700

    Init commit

---
+++ electron/number-clicker/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <NumberClicker/>
+</template>
+
+<script>
+import NumberClicker from './components/NumberClicker.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NumberClicker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20%;
+}
+</style>
---
+++ electron/number-clicker/src/components/NumberClicker.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h2> Number Clicker </h2>
+    <button v-on:click="number -= 1">-</button>
+    <h3> {{ number }} </h3>
+    <button v-on:click="number += 1">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberClicker',
+  data: function () {
+    return {
+      number: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  padding: 10px 20px;
+  border: 1px solid #008CBA;
+  color: #ffffff;
+  background-color: #008CBA;
+  border-radius: 4px;
+  font-size: 14px;
+  font-family: '微软雅黑',arail;
+  cursor: pointer;
+}
+h3 {
+  padding: 10px 30px;
+  display: inline;
+}
+</style>
